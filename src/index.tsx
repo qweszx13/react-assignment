ad126c596efd4e5289bf9b0cb7c7d462349174ad
キムドンギュ <kimdonggyu@bitA01500.local>
2024-04-19 10:14:39 +0900

    chore:create folder structure cereate-time:2024-04-19

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+let store = createStore(()=>{
+  return [{ id : 0, name : 'test', count : 4 }];
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store = {store}>
+      <App />
+    </Provider>    
   </React.StrictMode>
 );
 
