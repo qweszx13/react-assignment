a0aaa19ecc6ed9d158e6dda487c85b7f2ac1062a
Kimdonggyu <qweszx33@naver.com>
2024-04-21 15:57:55 +0900

    test:redux and github pagination create-time:2024-04-21 15:58

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
+import store from "./ contexts/store";
+
 function App() {
 
   const test = async ()=>{
-    const response = await fetch("https://api.github.com/repos/facebook/react/issues");
+    //Paging
+    const response = await fetch("https://api.github.com/repos/facebook/react/issues?page=1/issues?state=open");
     const jsonData = await response.json();
     console.log(jsonData);
   }
+
+  //store.dispatch()
   
   return (
     <div className="App">
-     <button onClick={()=>{test()}}></button>
+     <body>
+      <button onClick={()=>{test()}}></button>
+      <header>
+        <h1>Web</h1>
+        Hello Web
+      </header>
+      <nav>
+        <ol>
+          <li><a href="1.html">HTML</a></li>
+          <li><a href="2.css">CSS</a></li>
+        </ol>
+      </nav>
+      <article>
+        <ul>
+          <li><a href="/create">create</a></li>
+          <li><input type="button" value={"delete"}></input></li>
+        </ul>
+        <h2>HTML</h2>
+        HTML is...
+      </article> 
+     </body>
     </div>
   );
 }
