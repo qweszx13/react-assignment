719e64bb1d6f7110d3094ed0e29b9369246546b0
Kimdonggyu <qweszx33@naver.com>
2024-04-21 13:35:57 +0900

    test:create github api test code create-time:2024-04-21 13:36

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import './App.css';
-
 function App() {
+
+  const test = async ()=>{
+    const response = await fetch("https://api.github.com/repos/facebook/react/issues");
+    const jsonData = await response.json();
+    console.log(jsonData);
+  }
+  
   return (
     <div className="App">
-     
+     <button onClick={()=>{test()}}></button>
     </div>
   );
 }
