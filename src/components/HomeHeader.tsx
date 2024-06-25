274e9dc03b770c509fa300c82699db392c6d62d7
qweszx13 <qweszx33@naver.com>
2024-06-25 14:06:21 +0900

    test-code

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -4,7 +4,79 @@ import { setRepoParameter,setPageParameter } from "../contexts/apiParameterSlice
 import styles from "../styles/scss/home-header.module.scss"
 import { Dispatch, UnknownAction } from "redux";
 
+// // HomeHeader => ロジックと見た目を持ったコンポーネント
 
+// // React: ロジック、見た目は分けることができる
+// // React: ロジック => CustomHook閉じ込められる
+// // React: 見た目　=> JSX
+
+// // 分けることによるメリット
+// // Counter <=> useCounter
+
+// // カウンターコンポーネント
+// // => 見た目
+// // => ロジック
+
+// // => 見た目　=> JSX
+// // => ロジック => CustomHook
+
+// // 見た目、ロジックを分けていない例
+// const Counter = () => {
+//   // ロジック
+//   const [value, setCount] = useState(0)
+
+//   const handleClick = () => {
+//     setCount(current => current + 1)
+//   }
+
+//   const count = value
+
+  
+//   // 見た目
+//   return (
+//     <div onClick={handleClick}>
+//       {count}回ボタンが押された
+//     </div>
+//   )
+// }
+
+// // 見た目とロジックの分け方
+
+// // 見た目
+// const Counter = ({count, handleClick}) => (
+//   <div onClick={handleClick}>
+//     {count}回ボタンが押された
+//   </div>
+// )
+
+// // ロジック
+// const useCounter = () => {
+//   const [value, setCount] = useState(0)
+
+//   const countUp = () => {
+//     setCount(current => current + 1)
+//   }
+
+//   const count = value
+
+//   return ({
+//     count, 
+//     countUp,
+//   })
+// }
+
+// // 見た目とロジックをくっつける（合成する）
+// const Counter = () => {
+//   const {count, handleClick} = useCounter
+
+//   return <Counter count={count} handleClick={handleClick} />
+// }
+
+
+// 1. 見た目からロジックを抽出する
+// 2. どう的な値を引数として受け取れる形にする
+// 3. ロジックをcustomHookにする
+// 4. customHookと見た目を合成する
 export default function HomeHeader(){
   const [issuePageNumberValue, setIssuePageNumber] = useState(1);
   const [orgValue, setOrgValue] = useState<string>("");
@@ -21,27 +93,43 @@ export default function HomeHeader(){
     dispatch(setPageParameter(page));
   }
 
+  // event type 何を指定するんだ
+  const handleChangeOrg = (e) => setOrgValue(e.target.value);
+
+  const handleChangeRepo = (e) => setRepoValue(e.target.value);
+
+  const handleChangePageNumber = (e) => setIssuePageNumber(nanIsOne(e.target.value));
+  
+  const handleClickeLoadRepository = () => loadRepoOnclickEvent(orgValue, repoValue);
+
+  const handleClickPaging = () => jumpToPageOnclickEvent(issuePageNumberValue);
+
+  // styles, handleChangeOrg(e),handleChangeRepo(e),handleChangePage(e), handleClickeLoadRepository,handleClickPaging() 
+  
+  // 1. handle<EventName><ComponentName> <-- 案件ではよく見る
+  // 2. handle<ComponentName><EventName> <-- 英語表現上ではこっちの方が正しかった気がする。。
+
   return(
     <header className={styles.wrapper}>
       <div>
         <label>
           Org :
-          <input placeholder="Org" type="text" onChange={(e)=>setOrgValue(e.target.value)}/>
+          <input placeholder="Org" type="text" onChange={handleChangeOrg}/>
         </label>
         <label>
           Repo :
-          <input placeholder="Repo" type="text" onChange={(e)=>setRepoValue(e.target.value)}/>
+          <input placeholder="Repo" type="text" onChange={handleChangeRepo}/>
         </label>
-        <button onClick={()=>{loadRepoOnclickEvent(orgValue, repoValue)}}>
+        <button onClick={handleClickeLoadRepository}>
           Load Repo
         </button>
       </div>
       <div>
         <label>
           Issue Page :
-        <input placeholder="Issues Page ex)1,2,3" type="number" onChange={(e)=>setIssuePageNumber(nanIsOne(e.target.value))}/>
+        <input placeholder="Issues Page ex)1,2,3" type="number" onChange={handleChangePageNumber}/>
         </label>
-        <button onClick={()=>{jumpToPageOnclickEvent(issuePageNumberValue)}}>
+        <button onClick={handleClickPaging}>
           Jump to Page
         </button>
         <div data-testid="error-message">
