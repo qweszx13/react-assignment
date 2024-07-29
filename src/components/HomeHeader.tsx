d260b65281042b0f95f60c888124e410a6cb0671
qweszx13 <qweszx33@naver.com>
2024-07-29 17:20:10 +0900

    chore:Refactoring Home Header

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import {ChangeEvent, useState} from "react";
 import { useDispatch } from "react-redux";
 import { setRepoParameter,setPageParameter } from "../contexts/apiParameterSlice";
 import styles from "../styles/scss/home-header.module.scss"
@@ -31,7 +31,7 @@ import { Dispatch, UnknownAction } from "redux";
 
 //   const count = value
 
-  
+
 //   // 見た目
 //   return (
 //     <div onClick={handleClick}>
@@ -60,7 +60,7 @@ import { Dispatch, UnknownAction } from "redux";
 //   const count = value
 
 //   return ({
-//     count, 
+//     count,
 //     countUp,
 //   })
 // }
@@ -94,48 +94,48 @@ export default function HomeHeader(){
   }
 
   // event type 何を指定するんだ
-  const handleChangeOrg = (e) => setOrgValue(e.target.value);
+  const handleChangeOrg = (e: ChangeEvent<HTMLInputElement>) => setOrgValue(e.target.value);
 
-  const handleChangeRepo = (e) => setRepoValue(e.target.value);
+  const handleChangeRepo = (e: ChangeEvent<HTMLInputElement>) => setRepoValue(e.target.value);
 
-  const handleChangePageNumber = (e) => setIssuePageNumber(nanIsOne(e.target.value));
-  
+  const handleChangePageNumber = (e: ChangeEvent<HTMLInputElement>) => setIssuePageNumber(nanIsOne(e.target.value));
+
   const handleClickeLoadRepository = () => loadRepoOnclickEvent(orgValue, repoValue);
 
   const handleClickPaging = () => jumpToPageOnclickEvent(issuePageNumberValue);
 
-  // styles, handleChangeOrg(e),handleChangeRepo(e),handleChangePage(e), handleClickeLoadRepository,handleClickPaging() 
-  
+  // styles, handleChangeOrg(e),handleChangeRepo(e),handleChangePage(e), handleClickeLoadRepository,handleClickPaging()
+
   // 1. handle<EventName><ComponentName> <-- 案件ではよく見る
   // 2. handle<ComponentName><EventName> <-- 英語表現上ではこっちの方が正しかった気がする。。
 
   return(
-    <header className={styles.wrapper}>
-      <div>
-        <label>
-          Org :
-          <input placeholder="Org" type="text" onChange={handleChangeOrg}/>
-        </label>
-        <label>
-          Repo :
-          <input placeholder="Repo" type="text" onChange={handleChangeRepo}/>
-        </label>
-        <button onClick={handleClickeLoadRepository}>
-          Load Repo
-        </button>
-      </div>
-      <div>
-        <label>
-          Issue Page :
-        <input placeholder="Issues Page ex)1,2,3" type="number" onChange={handleChangePageNumber}/>
-        </label>
-        <button onClick={handleClickPaging}>
-          Jump to Page
-        </button>
-        <div data-testid="error-message">
-          error message sample
+      <header className={styles.wrapper}>
+        <div>
+          <label>
+            Org :
+            <input placeholder="Org" type="text" onChange={handleChangeOrg}/>
+          </label>
+          <label>
+            Repo :
+            <input placeholder="Repo" type="text" onChange={handleChangeRepo}/>
+          </label>
+          <button onClick={handleClickeLoadRepository}>
+            Load Repo
+          </button>
         </div>
-      </div>
-    </header>
+        <div>
+          <label>
+            Issue Page :
+            <input placeholder="Issues Page ex)1,2,3" type="number" onChange={handleChangePageNumber}/>
+          </label>
+          <button onClick={handleClickPaging}>
+            Jump to Page
+          </button>
+          <div data-testid="error-message">
+            error message sample
+          </div>
+        </div>
+      </header>
   )
 }
